ad5ad1294060379fb3a8d330c56f2e0f2a9528f6
Boranget <boranget@outlook.com>
2023-10-10 10:14:41 +0800

    修改脚本注释

--- dep.bat
+++ dep.bat
@@ -1,5 +1,5 @@
 git add .
 git commit -m%1
 git push origin master
-# 改为远程构建
-# hexo clean && hexo deploy
+:: 改为远程构建
+:: hexo clean && hexo deploy
--- pull.bat
+++
@@ -1,5 +0,0 @@
-git add .
-git commit -m%1
-git push origin master
-# 改为远程构建
-# hexo clean && hexo deploy
